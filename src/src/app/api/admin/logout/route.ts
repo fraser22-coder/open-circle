08f7230822262411b00cced8c4c62a1e533fc8d0
fraser22-coder <opencirclemarkets@gmail.com>
2026-05-04 11:39:11 +1200

    Delete src/src directory

--- src/src/app/api/admin/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { buildLogoutCookie } from '@/lib/adminAuth'
-
-export async function POST() {
-  const res = NextResponse.json({ success: true })
-  res.headers.set('Set-Cookie', buildLogoutCookie())
-  return res
-}
